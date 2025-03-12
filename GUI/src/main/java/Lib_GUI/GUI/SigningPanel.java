a7da6eb42e42e6823fd4e4e60b4e0b48436d53a2
paladin-2024 <161815974+paladin-2024@users.noreply.github.com>
2025-03-12 21:10:00 +0300

    Add SigningPanel form and initial implementation in SigningPanel class

--- GUI/src/main/java/Lib_GUI/GUI/SigningPanel.java
+++ GUI/src/main/java/Lib_GUI/GUI/SigningPanel.java
@@ -1,5 +1,12 @@
 package Lib_GUI.GUI;
 
+import javax.swing.*;
+
 public class SigningPanel {
 
+    private JPanel SigningPanel;
+    private JTextField usernameField;
+    private JTextField passwordField;
+    private JButton registerButton;
+    private JButton cancelButton;
 }
